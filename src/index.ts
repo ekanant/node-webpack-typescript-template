3a0a95ab1bd4994b05b59c1c132f665c93c809e2
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2019-02-15 15:37:39 +0700

    Implement simple expressjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world, Typescript by webpack");
+import * as express from "express";
+import * as compression from "compression";
+
+const app = express();
+
+const port: number = 3000;
+
+app.use(compression());
+app.get("/", async (req: express.Request, res: express.Response) => {
+  res.json({
+    text: "Hello world"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
